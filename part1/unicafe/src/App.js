cb751e6301c6e25a126de67cd742f39431398847
zhen_tu <yg@zhentu.work>
2024-04-30 16:53:54 +0800

    Exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,26 +2,29 @@ import { useState } from 'react';
 
 const Button = ({ text, handleClick }) => <button onClick={handleClick}> {text}</button>;
 const StatisticLine = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 const Statistic = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
-  const average = (good - bad) / all;
-  const positive = good / all;
+  const average = ((good - bad) / all).toFixed(1) || 0;
+  const positive = ((good / all) * 100).toFixed(1) || 0;
   return (
     <>
       <h1>statistics</h1>
       {all > 0 ? (
-        <>
-          <StatisticLine text='good' value={good} />
-          <StatisticLine text='neutral' value={neutral} />
-          <StatisticLine text='bad' value={bad} />
-          <StatisticLine text='all' value={all} />
-          <StatisticLine text='average' value={average} />
-          <StatisticLine text='positive' value={positive} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='all' value={all} />
+            <StatisticLine text='average' value={average} />
+            <StatisticLine text='positive' value={positive + '%'} />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
