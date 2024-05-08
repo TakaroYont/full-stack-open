51756155f0b2f047d8ae816b3d93a2ccf589a5fb
zhen_tu <yg@zhentu.work>
2024-05-08 13:55:12 +0800

    Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
   const handleNewNameChange = (event) => setNewName(event.target.value);
+  const handleNewNumberChange = (event) => setNewNumber(event.target.value);
   const addPerson = (event) => {
     event.preventDefault();
     if (persons.some((person) => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName('');
   };
 
@@ -22,12 +24,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumberChange} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>{person.name + ' ' + person.number}</div>
       ))}
     </div>
   );
